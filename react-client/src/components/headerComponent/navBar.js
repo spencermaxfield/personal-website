1ff53f8fd212e7eb6d31265e8350657ec55ab8e3
spencermaxfield <36614511+spencermaxfield@users.noreply.github.com>
2018-08-05 13:40:32 -0400

    Initial tweaks to the nav bar

--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
+
+    // TODO: should we even have links? If we do a single page that scrolls with all the content, then there
+    // really isn't a "nav bar." Just a header.
     return (
       <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
+        <div className="name">Spencer Maxfield</div>
+        <div id="headerButtons">
+          <span className="navButton"><Link to="">Home</Link></span>
+          <span className="navButton"><Link to="">Mixing</Link></span>
+          <span className="navButton"><Link to="">Music</Link></span>
+          <span className="navButton"><Link to="">Contact</Link></span>
+        </div>
       </header>
     )
   }
